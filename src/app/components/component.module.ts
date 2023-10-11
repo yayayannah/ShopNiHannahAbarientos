adab5a637344ea916654b91a5218287733d96bc3
Hannah Beatriz V. Abarientos <abarientoshannah24@gmail.com>
2023-10-12 00:22:32 +0800

    added new empty components: login and registration page

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 
 @NgModule({
   declarations: [
-    CardComponent
+    CardComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     CommonModule
